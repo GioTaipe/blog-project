6d50793d41ee1c96ee84977c4971c1d9bfb7fb4a
GioTaipe <120596675+GioTaip@users.noreply.github.com>
2025-08-08 12:52:17 +0200

    package

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,6 +6,6 @@ const PORT = 3001;
 // Conectar a la base de datos antes de iniciar el servidor
 connectDB().then(() => {
   app.listen(PORT, () => {
-    console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`);
+    console.log(`🚀 Servidor corriendo`);
   });
 });
