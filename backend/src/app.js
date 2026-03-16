ee9f5e6bf9483742de01d9a4adc59ab497205fb5
GioTaipe <120596675+GioTaip@users.noreply.github.com>
2026-03-16 20:44:05 +0100

    trigger redeploy

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,10 +9,6 @@ const errorHandler = require('./middleware/errorHandler')
 
 const app = express();
 app.use(cors());
-app.use((req, res, next) => {
-  res.setHeader('Cross-Origin-Opener-Policy', 'same-origin-allow-popups');
-  next();
-});
 app.use(express.json());
 app.use(fileUpload({
     useTempFiles: false,
